03bf9459df95c13ee52a6676ebeb60249f0642c0
ManaswiniN234 <manaswini.n234@gmail.com>
2025-10-22 12:24:14 +0530

    connected to mongoDB successfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import notesRoutes from './src/routes/notesRoutes.js';
+import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
+dotenv.config();
+
 const app = express();
+connectDB();
 
 app.use("/api/notes", notesRoutes)
-
 app.listen (3000, ()=>{
     console.log('port running at 3000')
 })
